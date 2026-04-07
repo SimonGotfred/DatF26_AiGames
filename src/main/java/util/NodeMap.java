c69bdb93866419bd7028bed4e8813709ec97de9e
Simon <sigo0001@stud.ek.dk>
2026-04-07 12:48:47 +0200

    update to NodeMap

--- src/main/java/util/NodeMap.java
+++ src/main/java/util/NodeMap.java
@@ -11,12 +11,26 @@ import java.util.List;
 
 public class NodeMap<T extends NodeMap.Node<T>> extends ConcurrentSkipListMap<T,T> // todo: testing & cleanup
 {
-    private static final    HashMap<Class<? extends Node<?>>, NodeMap<?>> clients = new HashMap<>();
-    private static boolean register(Class<? extends Node<?>> c){return clients.putIfAbsent(c, new NodeMap<>())==null;}
-    public static int   size(Class<?> c){return clients.containsKey(c) && clients.get(c) != null ? clients.get(c).size() : -1;}
-    public static void clear(Class<?> c){   if (clients.containsKey(c) && clients.get(c) != null)  clients.get(c).clear();}
-    public static <T extends Node<?>> void output(Class<T> c) {for (Node<?> node : clients.get(c).values()) node.output();}
-    static final String outputPath = "C:/Users/Simon/Documents/Obsidian/testing/"; // Markdown files parsable by Obsidian
+    private static final HashMap<Class<? extends Node<?>>, NodeMap<?>> clients = new HashMap<>();
+
+    private static <T extends Node<T>> boolean register(Class<T> c){return clients.putIfAbsent(c, new NodeMap<T>())==null;}
+    public  static <T extends Node<T>> int         size(Class<T> c){return clients.containsKey(c) && clients.get(c) != null ? clients.get(c).size() : -1;}
+    public  static <T extends Node<T>> void       clear(Class<T> c){   if (clients.containsKey(c) && clients.get(c) != null)  clients.get(c).clear();}
+
+    public  static <T extends Node<T>> T add   (T node) { T n = (T) of(node.getClass()).putIfAbsent(node, node); return n==null ? node : n;}
+    public  static <T extends Node<T>> T delete(T node) {return (T) of(node.getClass()).remove(node);}
+    public  static <T extends Node<T>> T get   (T node) {return add(node);}
+
+    // ! somewhat breaks for subclasses of *T* - as they will map to their own NodeMap
+    public  static <T extends Node<T>> NodeMap<T> of(Class<T> c)
+    {
+        NodeMap<T> map = (NodeMap<T>) clients.get(c);
+        if (map==null) clients.putIfAbsent(c,map = new NodeMap<>());
+        return map;
+    }
+
+    public  static <T extends Node<T>> void output(Class<T> c){for (Node<?> node : clients.get(c).values()) node.output();}
+    static  final String outputPath = "C:/Users/Simon/Documents/Obsidian/testing/"; // Markdown files parsable by Obsidian
 
     // ! Map instead of Set - to facilitate retrieving an *already present* node
     // ! to substitute *equal* nodes that are *not* the same Object in memory
@@ -24,12 +38,6 @@ public class NodeMap<T extends NodeMap.Node<T>> extends ConcurrentSkipListMap<T,
     //         should emulate "static" field per type 'T'
 //    public  NodeMap(Class<T> c) {register(c); map = map(c);}
     private NodeMap(){}          // {register((Class<T>)this.getClass()); map = map((Class<T>)this.getClass());} // only to be used by subclasses
-
-    public static  <T extends Node<T>> NodeMap<T> of(Class<T> c) {return (NodeMap<T>)clients.get(c);}
-
-    public  static <T extends Node<T>> T add   (T node) { T n = (T) of(node.getClass()).putIfAbsent(node, node); return n==null ? node : n;}
-    public  static <T extends Node<T>> T delete(T node) {return (T) of(node.getClass()).remove(node);}
-    public  static <T extends Node<T>> T get   (T node) {return add(node);} //
 
     public abstract static class Node<T extends Node<T>> implements Comparable<T>
     {
@@ -52,8 +60,8 @@ public class NodeMap<T extends NodeMap.Node<T>> extends ConcurrentSkipListMap<T,
 
         protected boolean removeChild (T child) {return children.remove( child);}
         protected boolean removeParent(T parent){return parents .remove(parent);}
-        protected boolean addParent   (T parent){return parents.add(parent);}
-        protected       T addChild    (T child)
+        public boolean addParent   (T parent){return parents.add(parent);}
+        public       T addChild    (T child)
         {
             child = add(child);  // substitute for potentially *equal* node already in map
             child.addParent((T)this);
